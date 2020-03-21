d78791f849c320c15af9db65a4253204503ac842
Thor77 <thor77@thor77.org>
2020-03-21 23:02:05 +0100

    Move helprequest detail to /helprequests/<id>

--- medRegApp/urls.py
+++ medRegApp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('register/helpseeker', views.register_help_seeker, name='register_help_seeker'),
     path('institutions/create', views.create_institution, name='create_institution'),
     path('institutions/<int:pk>/', views.InstitutionDetailView.as_view(), name='institution_detail'), 
-    path('helprequest/<int:pk>', views.HelpRequestDetailView.as_view(), name='help_request_detail'),
+    path('helprequests/<int:pk>', views.HelpRequestDetailView.as_view(), name='help_request_detail'),
     path('results', views.results, name='results'),
     path('users/', include('django.contrib.auth.urls')),
     path('helprequests/create', views.create_help_request, name='create_help_request'),
