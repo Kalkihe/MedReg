d06fef5983562ccee34f3d916e6a6cab1a5ab6c2
Thor77 <thor77@thor77.org>
2020-03-21 11:31:51 +0100

    Add database models

--- MedReg/settings.py
+++ MedReg/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'phonenumber_field',
+    'location_field.apps.DefaultConfig',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +83,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+AUTH_USER_MODEL = 'medRegApp.models.CustomUser'
 
 
 # Password validation
--- medRegApp/models.py
+++ medRegApp/models.py
@@ -1,4 +1,69 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
 
-# Create your models here.
+# Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+# Models
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20)
+    comment = models.TextField(max_length=500)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+class Helper(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    city = models.CharField(max_length=255)
+    location = PlainLocationField(based_fields=['city'])
+    is_available = models.BooleanField(default=True)
+
+
+class HelpSeeker(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    institution = models.CharField(max_length=100)
+
+
+class Qualification(models.Model):
+    helper = models.ForeignKey(Helper, on_delete=models.CASCADE)
