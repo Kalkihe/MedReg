75269a01a07fef5e8dcec0cfdf275f7fc3007e53
JP1998 <littlebigjonny06@gmail.com>
2020-03-21 12:05:43 +0100

    Add initial and basic templates

--- MedReg/settings.py
+++ MedReg/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'MedReg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'medRegApp/templates/'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
