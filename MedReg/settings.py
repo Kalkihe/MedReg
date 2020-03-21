036bcd4cba4fc24cc8dee9562598400e50ca3d0c
Thor77 <thor77@thor77.org>
2020-03-21 23:33:38 +0100

    Set language to de-DE

--- MedReg/settings.py
+++ MedReg/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-DE'
 
 TIME_ZONE = 'UTC'
 
