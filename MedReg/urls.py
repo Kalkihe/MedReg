a83f6c5542508f79f7b5c7e520ee481906da72d9
Thor77 <thor77@thor77.org>
2020-03-21 10:14:14 +0100

    Fix indention

--- MedReg/urls.py
+++ MedReg/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-		path('', include('medRegApp.urls')),
+    path('', include('medRegApp.urls')),
     path('admin/', admin.site.urls),
 ]
